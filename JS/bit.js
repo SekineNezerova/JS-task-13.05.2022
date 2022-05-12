cbbb86d9ba38b0353f5a007da1d8fedb735ad4d4
SekineNezerova <7b4gz6k@code.edu.az>
2022-05-12 13:55:55 -0700

    commited

---
+++ JS/bit.js
@@ -0,0 +1,84 @@
+// -------string length-------
+
+
+// let str="asdfghklitrufd"
+// console.log(str.length);
+
+
+
+// -------string slice-------
+
+
+// let str="alma, armud ,heyva, nar"
+// console.log(str.slice(6,11));
+
+// let str="alma, armud ,heyva, nar"
+// console.log(str.slice(6));
+
+
+// -------string substr-------
+
+
+// let str="alma, armud ,heyva, nar"
+// console.log(str.substr(5,15));
+
+
+// -------string uppercase-------
+
+
+// let text="string to uppercase"
+// console.log(text.toUpperCase());
+
+
+// -------string lowercase-------
+
+
+// let text="STRING TO LOWERCASE"
+// console.log(text.toLowerCase());
+
+
+// -------string concat-------
+
+
+// let text1="Java"
+// let text2="Script"
+// let text3 = text1.concat(" ",text2);
+// console.log(text3);
+
+
+// -------string Trim-------
+
+
+// let text1="         asdasdasd...     "
+// let text2=text1.trim()
+// console.log(text2);
+
+
+// -------string Padding-------
+
+
+// let txt1="9"
+// console.log(txt1.padStart(3,"-"));
+
+
+// let txt1="7"
+// console.log(txt1.padEnd(4,"@"));
+
+
+// -------string Characters-------
+
+
+// let txt1="Characters"
+// console.log(txt1.charAt(2));
+
+
+// let txt1="Characters"
+// console.log(txt1.charCodeAt(1));
+
+
+// -------string Split-------
+
+
+// let text1="asd.fgh.tyt.vyz"
+// let Array=text1.split(".")
+// console.log(Array[2]);
---
+++ JS/fibonacciPrompt.js
@@ -0,0 +1,15 @@
+// let res=prompt("fibonacci n-ci ededini daxil edin:")
+// function fibonacci(number){
+//     let number1=0;
+//     let number2=1;
+//     let sum;
+    
+//     for (let i = 0; i < number; i++) {
+//       sum=number1+number2;
+//       number1=number2;
+//       number2=sum;
+        
+//     }
+//     return number2
+// }
+// console.log(fibonacci(res));
